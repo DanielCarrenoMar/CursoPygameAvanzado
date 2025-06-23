58e88786ba401d2b34d8c64fd5b665d3b058b5a0
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-06-23 14:12:15 -0400

    feat y doc: imprimir matriz y poner comentarios

--- main.py
+++ main.py
@@ -13,30 +13,31 @@ class App:
         self.windows = py.display.set_mode((1024, 600), py.RESIZABLE)
         py.display.set_caption('Curso Pygame Avanzado')
 
-        self.clock = py.time.Clock()
-        theme = "data\\themes\\myTheme.json"
-        self.manager = pyui.UIManager((1024, 600), theme)
-        self.screenManager = ScreenManager(self.manager)
+        self.clock = py.time.Clock() # Creamos el reloj para controlar el tiempo
+        theme = "data\\themes\\myTheme.json" # Ubicacion del tema
+        self.manager = pyui.UIManager((1024, 600), theme) # Creamos el administrador de la interfaz de usuario
+        self.screenManager = ScreenManager(self.manager) # Creamos el administrador de pantallas
 
-        self.screenManager.addScreen(ScreenList.MAIN_MENU, MainMenuScreen)
+        self.screenManager.addScreen(ScreenList.MAIN_MENU, MainMenuScreen) # Agregamos la pantalla del menu principal
+        # Para agregar una pantalla se le pasa un int que sera su id y una clase para que el administrador la maneje
         self.screenManager.addScreen(ScreenList.GAME, GameScreen)
 
-        self.screenManager.show(ScreenList.MAIN_MENU)
-        self.running = True
+        self.screenManager.show(ScreenList.MAIN_MENU) # Mostramos la pantalla del menu principal al iniciar
+        self.running = True # Variable para controlar el bucle principal de la aplicacion
 
     def run(self):
         while self.running:
-            time_delta = self.clock.tick(60) / 1000.0
+            time_delta = self.clock.tick(60) / 1000.0 # Controlamos el tiempo entre frames, 60 FPS
 
             for event in py.event.get():
                 if event.type == py.QUIT:
                     self.running = False
 
-                self.manager.process_events(event)
-                self.screenManager.processEvents(event)
+                self.manager.process_events(event) # Procesa los eventos de la interfaz de usuario
+                self.screenManager.processEvents(event) # Procesa los eventos de la pantalla actual
 
-            self.manager.update(time_delta)
-            self.screenManager.update(time_delta)
+            self.manager.update(time_delta) # Actualiza la interfaz de usuario
+            self.screenManager.update(time_delta) # Actualiza la pantalla actual
             self.windows.fill((40, 40, 40)) # Manda a rellenar toda la ventana con un color
             self.manager.draw_ui(self.windows) # Manda a dibuja los elementos de la interfaz
             self.screenManager.draw(self.windows) # Manda a dibujar la pantalla actual
--- screens/gameScreen.py
+++ screens/gameScreen.py
@@ -1,4 +1,4 @@
-from pygame_gui.elements import UIButton, UILabel, UITextEntryBox
+from pygame_gui.elements import UIButton, UILabel
 import pygame as py
 import pygame_gui as pyui
 import numpy as np
@@ -10,14 +10,29 @@ class GameScreen(Screen):
     def __init__(self, uiManager: pyui.UIManager, screenManager: ScreenManager):
         self.screenManager = screenManager
         self.uiManager = uiManager
-        self.goToMainMenuButton = UIButton(relative_rect=py.Rect((400, 250), (200, 50)),
+        self.goToMainMenuButton = UIButton(relative_rect=py.Rect((-300, 250), (300, 50)),
+            anchors={"right": "right"},
             text='Ir a Menu principal',
             manager=uiManager)
-        self.titleLabel = UILabel(relative_rect=py.Rect((350, 150), (300, 50)),
+        self.titleLabel = UILabel(relative_rect=py.Rect((-300, 150), (300, 50)),
+            anchors={"right": "right"},
             text='Página de Juego',
             manager=uiManager)
         
         self.mat = np.zeros((3, 3), dtype=int)
+        self.mat[0, 0] = 1
+        self.mat[1, 1] = 2
+        self.mat[2, 2] = 1
+        print(self.mat)
+
+        # Cargar imágenes para las casillas
+        self.img_empty = py.image.load('data/images/space/space_1.jpg')
+        self.img_1 = py.image.load('data/images/space/space_2.jpg')
+        self.img_2 = py.image.load('data/images/space/space_3.jpg')
+        # Redimensionar imágenes a 50x50
+        self.img_empty = py.transform.scale(self.img_empty, (100, 100))
+        self.img_1 = py.transform.scale(self.img_1, (100, 100))
+        self.img_2 = py.transform.scale(self.img_2, (100, 100))
 
     def processEvents(self, event:py.event.Event):
         if event.type == pyui.UI_BUTTON_PRESSED and event.ui_element == self.goToMainMenuButton:
@@ -29,12 +44,14 @@ class GameScreen(Screen):
     def draw(self, surface: py.Surface):
         for i, row in enumerate(self.mat):
             for j, value in enumerate(row):
-                x = 100 + j * 60
-                y = 100 + i * 60
+                x = 100 + j * 120
+                y = 100 + i * 120
                 if value == 0:
-                    py.draw.rect(surface, (255, 255, 255), (x, y, 50, 50))
+                    surface.blit(self.img_empty, (x, y))
+                elif value == 1:
+                    surface.blit(self.img_1, (x, y))
                 else:
-                    py.draw.circle(surface, (0, 0, 255), (x + 25, y + 25), 20)
+                    surface.blit(self.img_2, (x, y))
 
     def destroy(self):
         self.goToMainMenuButton.kill()
